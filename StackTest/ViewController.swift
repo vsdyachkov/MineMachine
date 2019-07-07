5f3efc095cdc9ffa22e97a027a98fb6adb1e990c
Виктор <v.dyachkov@isimplelab.com>
2019-07-08 01:33:05 +0300

    Initial Commit

---
+++ StackTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StackTest
+//
+//  Created by Виктор on 08/07/2019.
+//  Copyright © 2019 Виктор. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
